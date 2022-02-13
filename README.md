e8e286e2509a8fbd8fc0c0ee485d67cd30bae823
James Moreton <james_moreton@hotmail.co.uk>
2022-02-13 16:26:41 +0000

    Add some error handling

--- README.md
+++ README.md
@@ -1,1 +1,10 @@
-## Tweet Gen
+[![Heroku App Status](http://heroku-shields.herokuapp.com/tweet-g)](https://tweet-g.herokuapp.com)
+
+# Tweet Gen
+
+A web app allowing you to create unique Tweets in the style of any Twitter user. Utilises a Markov chain generator within the domain of natural language processing.
+
+- Python backend; HTML/CSS/JavaScript frontend
+- [Flask](https://flask.palletsprojects.com/en/1.1.x/) framework
+- Uses [Twitter API](https://developer.twitter.com/en)
+- Deployed on [Heroku](https://www.heroku.com/home)
--- app.py
+++ app.py
@@ -1,5 +1,6 @@
-from flask import Flask, render_template, request
 import generator
+import re
+from flask import Flask, render_template, request
 
 app = Flask(__name__)
 
@@ -10,21 +11,27 @@ def index():
 
 @app.route("/gen")
 def gen():
-    username = request.args.get("username")
-    generate = request.args.get("generate")
+    query_username = request.args.get("username")
+    generate_username = request.args.get("generate")
 
-    if username:
-        user = generator.fetch_user(username)  # todo validate input...
+    if query_username:
+        if not valid(query_username):
+            return render_template("gen.html", step="START", username=query_username, invalid=True), 400
+
+        user = generator.fetch_user(query_username)
         if user:
             return render_template("gen.html", step="USER", user=user)
-        return render_template("gen.html", step="USER", username=username)
+        return render_template("gen.html", step="START", username=query_username)
 
-    if generate:
-        generated_tweets = generator.generate(generate)
+    if generate_username:
+        if not valid(generate_username):
+            return render_template("gen.html", step="START", username=generate_username, invalid=True), 400
+
+        generated_tweets = generator.generate(generate_username)
         return render_template(
             "gen.html",
             step="GENERATE",
-            username=generate,
+            username=generate_username,
             generated_tweets=generated_tweets
         )
 
@@ -36,7 +43,15 @@ def about():
 
 @app.errorhandler(Exception)
 def handle_exception(e):
-    return render_template("gen.html", step="ERROR", e=e), 500
+    print(e)
+    return render_template("error.html", reason="GENERIC", e=e), 500
+
+@app.errorhandler(404)
+def page_not_found(e):
+    return render_template("error.html", reason="NOT_FOUND", e=e), 404
+
+def valid(username):
+    return re.match(r"^[a-zA-Z0-9_]{1,15}$", username)
 
 if __name__ == '__main__':
-    app.run()
+    app.run(debug=True)
